9f990c9fa952bcc97d96930e8f62b5022baca796
juanma3331 <juanma3331@gmail.com>
2025-09-26 15:20:41 -0500

    agregacion de los demas modulos

--- JavaScript/generacion_reportes.js
+++
@@ -1,7 +0,0 @@
-/* Aqui debemos programar la consulta del historial
-de medicamentos por fechas.
-
-generacion de estadisticas en porcentaje de adherencia,
-dosis tomadas, omitidas, etc.
-
-Mostrar los reportes en tabla y/o exportar a pdf*/

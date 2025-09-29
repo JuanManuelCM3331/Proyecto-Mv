66ef0dfad4993f4832f14e4406678f8c409c9a4c
juanma3331 <juanma3331@gmail.com>
2025-09-29 06:30:57 -0500

    modificaciones varias

---
+++ JavaScript/config.js
@@ -0,0 +1,29 @@
+function guardarConfiguracion() {
+  const tamañoFuente = document.getElementById("tamañoFuente").value;
+  const modoContraste = document.getElementById("contraste").checked;
+
+  localStorage.setItem("tamañoFuente", tamañoFuente);
+  localStorage.setItem("modoContraste", modoContraste);
+
+  alert("Configuración guardada ✅");
+  location.reload()
+}
+
+function aplicarConfiguracion() {
+  const tamañoFuente = localStorage.getItem("tamañoFuente");
+  const modoContraste = localStorage.getItem("modoContraste") === "true";
+
+  if (tamañoFuente) {
+    document.body.style.fontSize =
+      tamañoFuente === "small" ? "14px" :
+      tamañoFuente === "medium" ? "18px" :
+      "22px";
+  }
+
+  if (modoContraste) {
+    document.body.classList.add("bg-black", "text-white");
+  } else {
+    document.body.classList.remove("bg-black", "text-white");
+  }
+}
+window.onload = aplicarConfiguracion;
